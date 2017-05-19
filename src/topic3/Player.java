f5c1f026a1b2bf6b769c9b481e318eecfb682074
Akhsan <Akhsan@DESKTOP-D29G6KB>
2017-05-19 17:31:42 +0700

    CsLul

---
+++ src/topic3/Player.java
@@ -0,0 +1,15 @@
+package topic3;
+public class Player {
+    public String id;
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public Player(String id){
+        this.id = id;
+    }
+    
+}
