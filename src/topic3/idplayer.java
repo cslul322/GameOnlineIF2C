3a39ad328c15364c70397d5fe2d04de77ed74685
cslul322 <akhsan2347@gmail.com>
2017-05-20 17:43:30 +0700

    Update idplayer.java

--- src/topic3/idplayer.java
+++ src/topic3/idplayer.java
@@ -1,1 +1,13 @@
-
+package topic3;
+public class idplayer{
+ public String id;
+  public String getid(){
+  return id;
+  }
+  public void setid(String id){
+  this.id = id;
+  }
+  public idplayer(String id){
+    this.id = id;
+  }
+}
