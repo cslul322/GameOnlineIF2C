c5aa4475f323f59d1696664a2e41fef09d78f1cb
dimaszelo <dimaszeloyudistira@gmail.com>
2017-05-20 20:18:29 +0700

    Update ClanCoC.java

--- src/topic3/ClanCoC.java
+++ src/topic3/ClanCoC.java
@@ -1,1 +1,40 @@
+package topic3;
 
+public class AnggotaClan;
+    public String clan1;
+    public String clan2;
+    public String clan3;
+
+    public void clan1(){
+      idplayer c1 = new idplayer();
+      Pasukan cc1 = new Pasukan();
+      System.out.println("Clan Sleysher");
+      System.out.println("Palyer\t\t\t"+"Trophy");
+      c1.cln1();
+      System.out.println("-----------------------------------");
+      System.out.println("Pasukan yang dimiliki Clan Sleysher");
+      cc1.clan1;
+    }
+
+    public void clan2(){
+      idplayer c2 = new idplayer();
+      Pasukan cc2 = new Pasukan();
+      System.out.println("Clan Imba");
+      System.out.println("Player\t\t\t"+Trophy);
+      c2.cln2();
+      System.out.println("-----------------------------------");
+      System.out.println("Pasukan yang dimiliki Clan Imba");
+      cc2.clan2;
+    }
+
+    public void clan3(){
+      idplayer c3 = new idplayer();
+      Pasukan cc3 = new Pasukan();
+      System.out.println("Clan Csgo");
+      System.out.println("Palyer\t\t\t"+"Trophy");
+      c3.cln3();
+      System.out.println("-----------------------------------");
+      System.out.println("Pasukan yang dimiliki CLan Csgo");
+      cc3.clan3();
+    }
+  
