89eab7450c725516fcd5c5897e7b6fd3fcb523ad
Taruna Balong Islami <islami9798@gmail.com>
2017-05-20 08:20:22 -0400

    Update Pasukan.java

--- src/topic3/Pasukan.java
+++ src/topic3/Pasukan.java
@@ -1,1 +1,49 @@
-
+package topic3;
+public class ClanCoC {
+    public int jum;
+    public int jum2;
+    public int jum3;
+    public void clan1(){
+        int a = 45 ;
+        System.out.println("Barbarian   "+"Atk Power"+a);
+        int b= 50;
+        System.out.println("Archer      "+"Atk Power"+b);
+        int c= 70;
+        System.out.println("Dragon      "+"Atk Power"+c);
+        int d= 65;
+        System.out.println("Hog         "+"Atk Power"+d);
+        int e= 55;
+        System.out.println("Ballon      "+"Atk Power"+e);
+        jum = a+b+c+d+e;
+        System.out.println("Jumlah Atk Power "+jum);
+    }
+    public void clan2(){
+        int a= 68;
+        System.out.println("Arthas          "+"Atk Power"+a);
+        int b= 55;
+        System.out.println("Lich King       "+"Atk Power"+b);
+        int c=70;
+        System.out.println("Muradin         "+"Atk Power"+c);
+        int d=85;
+        System.out.println("Illidan         "+"Atk Power"+d);
+        int e=75;
+        System.out.println("Tyrande         "+"Atk Power"+e);
+        jum2 = a+b+c+d+e;
+        System.out.println("Jumlah Atk Power"+jum2);
+    }
+    public void clan3(){
+        int a=79;
+        System.out.println("Baby Dragon         "+"Atk Power"+a);
+        int b=67;
+        System.out.println("Minnions            "+"Atk Power"+b);
+        int c=69;
+        System.out.println("Giant               "+"Atk Power"+c);
+        int d=76;
+        System.out.println("Pekka               "+"Atk Power"+d);
+        int e=10;
+        System.out.println("Marsupilami         "+"Atk Power"+e);
+        jum3 = a+b+c+d+e;
+        System.out.println("Jumlah Atk Power"+jum3);
+    }
+    
+}
